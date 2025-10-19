16a2de1632c93f144dcda6a79e319513d6205b41
Nickolay Petroff <geniuss0ft@yahoo.com>
2025-10-19 11:21:21 -0700

    Add Content Security Policy headers to allow Google Fonts and external resources

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,29 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline'",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+              "font-src 'self' https://fonts.gstatic.com",
+              "img-src 'self' data: https: blob:",
+              "connect-src 'self' https: wss:",
+              "worker-src 'self' blob:",
+              "frame-src 'self'",
+              "manifest-src 'self'"
+            ].join('; ')
+          }
+        ]
+      }
+    ];
+  }
 };
 
 export default withNextIntl(nextConfig);
